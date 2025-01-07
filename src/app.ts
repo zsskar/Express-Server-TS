32dac62332d0c8faf431138ff9f9249ff0faed1f
Rashid Ali Khan <rashidali.khan.-ND@disneystreaming.com>
2025-01-07 11:53:07 +0530

    added base url api and all api's collection in the api

--- src/app.ts
+++ src/app.ts
@@ -1,12 +1,15 @@
-import  express  from 'express';
+import express, { Request, Response } from 'express';
 import routes from './routes/index';
 import { errorHandler } from './error/handler';
+import { rootPathMsg } from './utils/apisMeta';
 
 const app = express();
 
 app.use(express.json());
+app.use('/', (req: Request, res: Response) => {
+  res.json(rootPathMsg);
+});
 app.use('/api', routes);
 app.use(errorHandler);
-
 
 export default app;
--- src/routes/cart.ts
+++ src/routes/cart.ts
@@ -3,10 +3,10 @@ import Router from 'express';
 
 const cartRouter = Router();
 
-cartRouter.get("/:id/user/:userId", cartController.getCartProductByIdAndUser);
-cartRouter.get("/:userId", cartController.getCartProductsByUser);
-cartRouter.post("/:userId", cartController.addProductToCart);
-cartRouter.delete("/:userId", cartController.deleteCartProductByIdAndUser);
-cartRouter.delete("/:userId", cartController.deleteAllCartProductsByUser);
+cartRouter.get('/:id/user/:userId', cartController.getCartProductByIdAndUser);
+cartRouter.get('/:userId', cartController.getCartProductsByUser);
+cartRouter.post('/:userId', cartController.addProductToCart);
+cartRouter.delete('/:userId', cartController.deleteCartProductByIdAndUser);
+cartRouter.delete('/:userId', cartController.deleteAllCartProductsByUser);
 
 export default cartRouter;
