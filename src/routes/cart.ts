daa10c7c65ac89fea5c875005f85b5d89ef5890d
Rashid Ali Khan <rashid.11612612@gmail.com>
2025-01-10 17:00:08 +0530

    added middleware in cart api's

--- src/routes/cart.ts
+++ src/routes/cart.ts
@@ -1,12 +1,29 @@
+import { authMiddleware } from '../auth/authMiddleware';
 import * as cartController from '../controllers/cartController';
 import Router from 'express';
 
 const cartRouter = Router();
 
-cartRouter.get('/:id/user/:userId', cartController.getCartProductByIdAndUser);
-cartRouter.get('/:userId', cartController.getCartProductsByUser);
-cartRouter.post('/:userId', cartController.addProductToCart);
-cartRouter.delete('/:userId', cartController.deleteCartProductByIdAndUser);
-cartRouter.delete('/:userId', cartController.deleteAllCartProductsByUser);
+cartRouter.get(
+  '/:id/user/:userId',
+  authMiddleware,
+  cartController.getCartProductByIdAndUser,
+);
+cartRouter.get(
+  '/:userId',
+  authMiddleware,
+  cartController.getCartProductsByUser,
+);
+cartRouter.post('/:userId', authMiddleware, cartController.addProductToCart);
+cartRouter.delete(
+  '/:userId',
+  authMiddleware,
+  cartController.deleteCartProductByIdAndUser,
+);
+cartRouter.delete(
+  '/:userId',
+  authMiddleware,
+  cartController.deleteAllCartProductsByUser,
+);
 
 export default cartRouter;
