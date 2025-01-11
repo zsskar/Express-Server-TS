ba222f5c71e7e0e4430664b465cc7f517aac31ea
Rashid Ali Khan <rashid.11612612@gmail.com>
2025-01-11 23:20:56 +0530

    added middleware in the purchases api's

--- src/routes/purchase.ts
+++ src/routes/purchase.ts
@@ -1,13 +1,19 @@
+import { authMiddleware } from '../auth/authMiddleware';
 import * as purchaseController from '../controllers/purchaseController';
-import Router from 'express'
+import Router from 'express';
 
 const purchaseRouter = Router();
 
-purchaseRouter.post("/", purchaseController.createPurchase);
-purchaseRouter.get("/:userId", purchaseController.getAllPurchasesByUser);
-purchaseRouter.get("/:id/user/:userId", purchaseController.getPurchaseByIdAndUser);
-
+purchaseRouter.post('/', authMiddleware, purchaseController.createPurchase);
+purchaseRouter.get(
+  '/:userId',
+  authMiddleware,
+  purchaseController.getAllPurchasesByUser,
+);
+purchaseRouter.get(
+  '/:id/user/:userId',
+  authMiddleware,
+  purchaseController.getPurchaseByIdAndUser,
+);
 
 export default purchaseRouter;
-
-
