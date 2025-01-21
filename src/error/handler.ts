a8a37b24cefad7632467dca838c5c35d3f27bbf1
Rashid Ali Khan <rashid.11612612@gmail.com>
2025-01-21 13:25:37 +0530

    formatted code

--- src/error/handler.ts
+++ src/error/handler.ts
@@ -2,7 +2,12 @@ import { Request, Response, NextFunction } from 'express';
 import { ZodError } from 'zod';
 import { Prisma } from '@prisma/client';
 
-export const errorHandler = (err: any, req: Request, res: Response, next: NextFunction) : void => {
+export const errorHandler = (
+  err: any,
+  req: Request,
+  res: Response,
+  next: NextFunction,
+): void => {
   if (res.headersSent) {
     return next(err);
   }
@@ -13,18 +18,20 @@ export const errorHandler = (err: any, req: Request, res: Response, next: NextFu
       acc[key] = e.message; // Assign the message to the corresponding key
       return acc;
     }, {} as Record<string, string>); // Explicitly type the accumulator
-  
-     res.status(400).json({ error: formattedErrors });
+
+    res.status(400).json({ error: formattedErrors });
   }
 
   if (err instanceof Prisma.PrismaClientKnownRequestError) {
     if (err.code === 'P2002') {
-       res.status(400).json({ error: {email :'A user with this email already exists.' }});
+      res
+        .status(400)
+        .json({ error: { email: 'A user with this email already exists.' } });
     }
   }
 
   if (err instanceof Error) {
-     res.status(400).json({ error: err.message });
+    res.status(400).json({ error: err.message });
   }
 
   res.status(500).json({ error: 'Internal Server Error' });
